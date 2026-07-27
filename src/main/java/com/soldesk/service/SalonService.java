7b346104d3828992f5b388c2ffeb1530ed6c4291
sukyum <kyumi1003@naver.com>
2026-07-27 19:12:34 +0900

    search구현, 창모병합

--- src/main/java/com/soldesk/service/SalonService.java
+++ src/main/java/com/soldesk/service/SalonService.java
@@ -17,17 +17,17 @@ public class SalonService {
     private SalonMapper salonMapper;
 
     @Transactional(readOnly = true)
-    public List<SalonVO> getSalons() {
+    public List<SalonVO> getSalons() {  
         return salonMapper.findAllWithMinimumPrice();
-    }
+    }//모든 미용실정보 가져오기 평점 내림차순
 
     @Transactional(readOnly = true)
     public SalonVO getSalon(int salonId) {
         return salonMapper.findById(salonId);
-    }
+    }//ID로 미용실 정보 가져오기
 
     @Transactional(readOnly = true)
     public List<ServiceVO> getServices(int salonId) {
         return salonMapper.findServicesBySalonId(salonId);
-    }
+    }//ID로 시술정보 가져오기
 }
